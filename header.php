28e70deb1d7199f76e47358855fa0b53a88f0756
Bobby Holtzner <me@bobbyholtzner.com>
2016-01-11 20:12:20 -0500

    First Commit

---
+++ header.php
@@ -0,0 +1,41 @@
+<?php
+  // Main Header File for the blog
+ ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title><?php // TODO Echo out the title here ?></title>
+    <meta name="description" content="<?php // TODO echo out the description ?>">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Ubuntu:400,500,700,300,300italic,400italic,700italic,500italic' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="/css/main.css" media="screen" title="no title" charset="utf-8">
+  </head>
+  <body>
+    <header>
+      <div id="infoBar">
+        <div class="wrapper">
+          <div id="social">
+
+          </div>
+          <div id="contactInfo">
+
+          </div>
+        </div>
+      </div>
+      <nav>
+        <div class="wrapper">
+          <div id="logoContainer">
+            <a href="#"><img width="250" src="images/logo.png" alt=""></a>
+            <p>Design &amp; Development</p>
+          </div>
+          <ul class="nav">
+            <li><a href="#">Our Services</a></li>
+            <li><a href="#">Work</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </div>
+      </nav>
+    </header>
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php include('header.php'); ?>
+
+  <div id="hero">
+
+  </div>
+  <div id="whatWeDo">
+
+  </div>
+  <div id="ourWork">
+
+  </div>
+  <div id="homeCTA">
+    
+  </div>
+  <div id="about">
+
+  </div>
+
+<?php include('footer.php'); ?>
